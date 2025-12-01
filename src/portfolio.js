ec19706dbfdd71d7ec55cd5e40a4a1228a150d42
Owenonkwani99 <owenhempstone@gmail.com>
2025-12-01 13:24:27 +0300

    Improve structure and formatting

--- src/portfolio.js
+++ src/portfolio.js
@@ -1,17 +1,17 @@
 const header = {
   // all the properties are optional - can be left empty or deleted
-  homepage: 'https://rjshkhr.github.io/cleanfolio',
-  title: 'JS.',
+  homepage: 'https://Owenonkwani99.github.io/Portfolio',
+  title: 'OO.',
 }
 
 const about = {
   // all the properties are optional - can be left empty or deleted
   name: 'Owen Onkwani',
   role: 'Software Developer',
-  picture: 'https://github.githubassets.com/assets/GitHub-Mark-ea2971cee799.png',
+ // picture: 'https://github.githubassets.com/assets/GitHub-Mark-ea2971cee799.png',
 
   description:
-    'Software Developer with strong foundations in Java, OOP, data structures  algorithms, backend logic, and relational databases. Skilled in building  scalable, secure, and maintainable applications. Hands-on experience in API development, debugging, testing, and working in collaborative teams. A fast learner dedicated to delivering clean, efficient software solutions.',
+    'With strong foundations in Java, OOP, data structures  algorithms, backend logic, and relational databases. Skilled in building  scalable, secure, and maintainable applications. Hands-on experience in API development, debugging, testing, and working in collaborative teams. A fast learner dedicated to delivering clean, efficient software solutions.',
   resume: 'https://example.com',
   social: {
     linkedin: 'https://linkedin.com',
@@ -25,20 +25,20 @@ const projects = [
   {
     name: 'Project 1',
     description:
-      'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
-    stack: ['SASS', 'TypeScript', 'React'],
+      'HekimaBot is an intelligent Java-based chatbot that analyzes your documents and delivers context-aware responses using RAG and modern AI frameworks. Built with Vaadin, it offers a smooth interface and efficient real-time interactions.',
+    stack: ['Java', 'Spring Boot', 'Vaadin'],
     sourceCode: 'https://github.com',
     livePreview: 'https://github.com',
-    image: 'cleanfolio.png',
+    // image: 'cleanfolio.png',
   },
   {
     name: 'Project 2',
     description:
-      'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
-    stack: ['SASS', 'TypeScript', 'React'],
+      'A Java-based banking system built with Spring Boot, featuring secure account management, customer CRUD operations, deposits, withdrawals, and internal transfers. Designed as a RESTful API with built-in Swagger documentation and an H2 in-memory database for rapid development and testing.',
+    stack: ['Java', 'Spring Boot', 'Spring Security'],
     sourceCode: 'https://github.com',
     livePreview: 'https://github.com',
-    image: 'https://github.githubassets.com/assets/GitHub-Logo-ee398b662d42.png',
+   // image: 'https://github.githubassets.com/assets/GitHub-Logo-ee398b662d42.png',
   },
   {
     name: 'Project 3',
